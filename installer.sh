e9463adde41314d8111f341ec19ff49eead54227
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-23 01:50:25 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,19 +1,35 @@
 #!/bin/sh
 # ###########################################
 # SCRIPT : DOWNLOAD AND INSTALL tarek
-
 # ###########################################
-#  wget https://raw.githubusercontent.com/tarekzoka/tarek/main/installer.sh -qO - | /bin/sh  
-#!/bin/sh
 #
+# Command: wget https://raw.githubusercontent.com/tarekzoka/tarek/main/installer.sh -qO - | /bin/sh 
+#
+# ###########################################
 
-wget -O /tmp/channels_backup_user_2021_09_04.tar.gz "https://github.com/tarekzoka/tarek/raw/main/channels_backup_user_2021_09_04.tar.gz"
 
+###########################################
+MY_URL="https://raw.githubusercontent.com/tarekzoka/tarek/main/"  
+echo " remove old channel "
+# Remove any Channel  # 
+rm -rf /etc/enigma2/lamedb 
+rm -rf /etc/enigma2/*list 
+rm -rf /etc/enigma2/*.tv rm -rf /etc/enigma2/*.radio 
+rm -rf /etc/tuxbox/*.xml 
+echo " download and install channel "
+wget -O /var/volatile/tmp/channels_backup_romeh_2021_09_09.tar.gz "https://raw.githubusercontent.com/tarekzoka/tarek/main/channels_backup_romeh_2021_09_09.tar.gz"
 wait
-#!/bin/sh
-tar xzvpf /tmp/*.tar.gz  -C /
-wait
-tar xzvpf /tmp/*.tar.gz  -C /
+tar xzvpf /tmp/channels_backup_romeh_2021_09_09.tar.gz  -C /
 wait
 sleep 2;
-;
+echo "" 
+echo "" 
+echo "" "*********************************************************" 
+echo "# Channel And INSTALLED SUCCESSFULLY #"
+echo "          UPLOADED BY zz_DD "
+echo " "*********************************************************" 
+echo "# your Device will RESTART Now #" echo "****************************************"
+wait 2
+init 4
+init 3
+exit 0
