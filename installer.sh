1d841d969c69bf5b80a75dceea2a6f35b07542fc
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-23 02:05:40 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL tarek
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/tarekzoka/tarek/main/installer.sh -qO - | /bin/sh 
+#
+# ###########################################
+
+
+###########################################
+MY_URL="https://raw.githubusercontent.com/tarekzoka/tarek/main/"  
+echo " remove old channel "
+# Remove any Channel  # 
+rm -rf /etc/enigma2/lamedb 
+rm -rf /etc/enigma2/*list 
+rm -rf /etc/enigma2/*.tv rm -rf /etc/enigma2/*.radio 
+rm -rf /etc/tuxbox/*.xml 
+echo " download and install channel "
+wget -O /tmp/channels_backup_user_2021_09_04.tar.gz "https://raw.githubusercontent.com/tarekzoka/tarek/main/channels_backup_user_2021_09_04.tar.gz"
+wait
+tar xzvpf /tmp/channels_backup_user_2021_09_04.tar.gz  -C /
+wait
+sleep 2;
+echo "" 
+echo "" 
+echo "" "*********************************************************" 
+echo "# Channel And INSTALLED SUCCESSFULLY #"
+echo "          UPLOADED BY zz_DD "
+echo " "*********************************************************" 
+echo "# your Device will RESTART Now #" echo "****************************************"
+wait 2
+init 4
+init 3 
+exit 0
